52a9623cc7a42c94681ed9eee2d56767d9ae8094
Jaroslaw <zabuski.jaroslaw@gmail.com>
2021-03-29 16:24:40 +0200

    Adding business model RestController.

--- src/main/java/io/github/mat3e/model/Task.java
+++ src/main/java/io/github/mat3e/model/Task.java
@@ -1,13 +1,39 @@
 package io.github.mat3e.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 // TODO: stub test task to do
 @Entity
 @Table(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "desc")
+    @NotBlank(message = "Task's description must be not null.")
     private String description;
+    @Column(name = "done")
     private boolean done;
+
+    Task()
+    {
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
