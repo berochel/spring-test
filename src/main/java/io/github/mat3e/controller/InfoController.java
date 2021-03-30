24feaf41687deb2189476e6bdd5381b966f27558
Jaroslaw <zabuski.jaroslaw@gmail.com>
2021-03-30 12:06:03 +0200

    Adding remote h2 database access point - revision.

--- src/main/java/io/github/mat3e/controller/InfoController.java
+++ src/main/java/io/github/mat3e/controller/InfoController.java
@@ -1,4 +1,28 @@
 package io.github.mat3e.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class InfoController {
+
+    @Autowired
+    private DataSourceProperties dataSource;
+
+    @Value("${my.prop}")
+    private String myProp;
+
+    @GetMapping("/info/url")
+    String url()
+    {
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    String myProp(){
+        return myProp;
+    }
 }
